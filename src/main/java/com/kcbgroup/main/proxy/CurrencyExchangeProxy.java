847f5ac248a8ee70c70fc2784995348ee87b164a
KEN20957 <mercycherot1998@gmail.com>
2022-09-20 16:45:03 +0300

    initial updates

--- src/main/java/com/kcbgroup/main/proxy/CurrencyExchangeProxy.java
+++ src/main/java/com/kcbgroup/main/proxy/CurrencyExchangeProxy.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 /**
  * @ AUTHOR MKOECH
  **/
+
 //@FeignClient(name = "currency-exchange",url = "localhost:8000")
+
 @FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
